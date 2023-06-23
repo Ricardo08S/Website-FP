bcbb6f75c2116ecc3696157d1ca857844aad0558
Ricardo S <ricardo.supriyanto08@gmail.com>
2023-06-23 23:05:34 +0700

    feat: hasil website

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "2c5cdb292b17c27e"; }
+/******/ 		__webpack_require__.h = function() { return "28d7108e52ea32cd"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
